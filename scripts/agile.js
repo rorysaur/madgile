7e2af82f02607e2025d2b46518be1fdeb62f3ea0
rorysaur <ou.rory@gmail.com>
2014-09-29 22:40:48 -0700

    try changing file path for reals

--- scripts/agile.js
+++ scripts/agile.js
@@ -11,21 +11,11 @@ var fs = require('fs');
 var markov = require('markov');
 var m = markov();
 
-fs.readFile('sauce/agile.txt', 'utf8', function(err, data) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.split(' ').length + ' words read in agile.txt.');
-});
-
-var stream = fs.createReadStream(__dirname + '../sauce/agile.txt');
+var stream = fs.createReadStream(__dirname + 'sauce/agile.txt');
 
 module.exports = function(robot) {
   m.seed(stream, function() {
-    console.log('seeded');
     robot.hear(/^.*agile.*$/i, function(msg) {
-      console.log(m.respond(msg.match[0], 16));
-      msg.send('lol');
       msg.send(m.respond(msg.match[0], 16).join(' '));
     });
   });
