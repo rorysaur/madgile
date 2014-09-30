dc772a28f557b5cea5d36ed9cfc96be0df218e1a
rorysaur <ou.rory@gmail.com>
2014-09-29 23:58:19 -0700

    agile without markov chaining

--- scripts/agile.js
+++ scripts/agile.js
@@ -2,27 +2,27 @@
 //   Provides agile inspiration when relevant to the conversation.
 //
 // Dependencies:
-//   "markov": "0.0.7" 
+//   None
 //
 // Commands:
-//   agile - Returns a markov-chained sentence from agile sauce text.
+//   agile - Returns a sentence from agile sauce text.
 
 var fs = require('fs');
-var markov = require('markov');
-var m = markov();
 
 module.exports = function(robot) {
   fs.readFile('sauce/agile.txt', 'utf8', function(err, data) {
     if (err) {
       return console.log(err);
     }
-    console.log(data.split(' ').length + ' words read in agile.txt');
 
-    m.seed(data, function() {
-      console.log('sauce text seeded.');
-      robot.hear(/^.*agile.*$/i, function(msg) {
-        msg.send(m.respond(msg.match[0], 16).join(' '));
-      });
+    var sentences = data.split('\n');
+    var count = sentences.length;
+    console.log(count + ' sentences read in agile.txt.');
+    
+    robot.hear(/agile/i, function(msg) {
+      var idx = Math.floor(Math.random() * (count - 1));
+      var response = sentences[idx];
+      msg.send(response);
     });
   });
 };
