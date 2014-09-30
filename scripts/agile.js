38f88da4ac2225b278a77e63c347c6e0f2ed24ea
rorysaur <ou.rory@gmail.com>
2014-09-29 22:46:48 -0700

    try seeding with readFile

--- scripts/agile.js
+++ scripts/agile.js
@@ -11,12 +11,22 @@ var fs = require('fs');
 var markov = require('markov');
 var m = markov();
 
-var stream = fs.createReadStream(__dirname + 'sauce/agile.txt');
+// var stream = fs.createReadStream(__dirname + 'sauce/agile.txt');
 
 module.exports = function(robot) {
-  m.seed(stream, function() {
-    robot.hear(/^.*agile.*$/i, function(msg) {
-      msg.send(m.respond(msg.match[0], 16).join(' '));
+  fs.readFile('sauce/agile.txt', 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log(data.split(' ').length + ' words read in agile.txt');
+    m.seed(data, function() {
+      console.log('seeded');
+      robot.hear(/^.*agile.*$/i, function(msg) {
+        console.log(m.respond(msg.match[0], 16));
+        msg.send('lol');
+        msg.send(m.respond(msg.match[0], 16).join(' '));
+      });
     });
   });
 };
